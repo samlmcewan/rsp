69e408cb79ba327c0982574e3e85ea513aa56a63
samlmcewan <samlmcewan@gmail.com>
2022-10-16 17:50:52 +0100

    Create homepage hero slider

---
+++ scripts/slider.js
@@ -0,0 +1,90 @@
+
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  //  new Swiper('.sliderCarousel', {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  //   slidesPerView: 'auto',
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //     clickable: true
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   }
+  // });
+
+  /**
+   * Init swiper slider with 3 slides at once in desktop view
+   */
+  // new Swiper('.sliderCarousel', {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  //   slidesPerView: 'auto',
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //     clickable: true
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 40
+  //     },
+
+  //     1200: {
+  //       slidesPerView: 3,
+  //     }
+  //   }
+  // });
+              
+              
+              
+/**
+   * Init swiper slider with 2 slides at once in desktop view
+   */
+  new Swiper('.sliderCarousel', {
+    speed: 800,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      }
+    }
+  });      
+
