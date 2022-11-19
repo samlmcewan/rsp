4f2b8f3e727f01e0ebad009d5d395a82bd57c09a
samlmcewan <samlmcewan@gmail.com>
2022-11-19 14:16:57 +0000

    Create philosophy section on homepage

---
+++ scripts/angles.js
@@ -0,0 +1,11 @@
+// resize the philosophy angle bg to be x bigger than the container innerHeigh
+
+// const container = document.querySelector('.philosophy');
+// const angle = document.querySelector('.angle-bg');
+
+// console.log(angle);
+// const containerH =  container.offsetHeight;
+
+// const h = containerH + 'px';
+// console.log(h);
+// angle.style.height = h;
