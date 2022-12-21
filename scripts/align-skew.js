c12eae2f7e70a9c41797da01e7d5a03ad7076f2b
Sam McEwan <samlmcewan@nhs.scot>
2022-12-21 13:09:55 +0000

    SVG skew

--- scripts/align-skew.js
+++ scripts/align-skew.js
@@ -1,16 +1,16 @@
 // If we are on devices >= large devices, push the skew down below the main nav height 
-if (window.matchMedia('(min-device-width: 992px)').matches) {
-    let mainNav = document.querySelector('#main-nav .container-fluid');
+// if (window.matchMedia('(min-device-width: 992px)').matches) {
+//     let mainNav = document.querySelector('#main-nav .container-fluid');
 
-    let compNav = window.getComputedStyle(mainNav);
+//     let compNav = window.getComputedStyle(mainNav);
 
-    let navH = compNav.getPropertyValue('height');
+//     let navH = compNav.getPropertyValue('height');
 
-    let skewtop = document.querySelector('.skew-top');
+//     let skewtop = document.querySelector('.skew-top');
 
-    let compSkew = window.getComputedStyle(skewtop);
+//     let compSkew = window.getComputedStyle(skewtop);
 
 
-    skewtop.style.setProperty('margin-top', navH);
+//     skewtop.style.setProperty('margin-top', navH);
 
-                }
+//                 }
